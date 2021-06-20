9184690c006dd7708ab093ed4a154cfe99bc2680
Arie Peretz <ariep20@gmail.com>
2021-06-20 18:04:46 +0300

    Initial Commit

---
+++ DiffableDataSourceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiffableDataSourceApp
+//
+//  Created by Arie Peretz on 20/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
